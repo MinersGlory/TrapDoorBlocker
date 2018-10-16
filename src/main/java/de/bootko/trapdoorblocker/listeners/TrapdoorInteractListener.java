8068be6db0168d1238367316797b98517265c278
Zethrus <contact@zethr.us>
2018-10-16 07:33:57 -0600

    Add debug code

--- src/main/java/de/bootko/trapdoorblocker/listeners/TrapdoorInteractListener.java
+++ src/main/java/de/bootko/trapdoorblocker/listeners/TrapdoorInteractListener.java
@@ -40,13 +40,12 @@ public class TrapdoorInteractListener implements Listener {
 
         // If trapdoor then block item interact if in region "newhaven"
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_BLOCK) {
-            if (block.getType() == Material.getMaterial(trapdoors.toString())) {
-
+            player.sendMessage(trapdoors.toString()); // DEBUG
+            if (block.getType().equals(Material.getMaterial(trapdoors.toString()))) {
                 RegionContainer container = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getRegionContainer();
                 RegionQuery query = container.createQuery();
                 ApplicableRegionSet set = query.getApplicableRegions(BukkitAdapter.adapt(currentLocation));
                 event.setCancelled(true);
-                trapdoors.remove(currentLocation);
             }
         } else {
             // hmm
