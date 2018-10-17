5c3aef9641ec911a5eae6c0164af233238bcfbee
Zethrus <contact@zethr.us>
2018-10-17 12:44:59 -0600

    Check if can build

--- src/main/java/de/bootko/trapdoorblocker/listeners/TrapdoorInteractListener.java
+++ src/main/java/de/bootko/trapdoorblocker/listeners/TrapdoorInteractListener.java
@@ -51,9 +51,12 @@ public class TrapdoorInteractListener implements Listener {
                             LocalPlayer wgPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
                             if (!StateFlag.test(WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery().queryState(BukkitAdapter.adapt(currentLocation), (RegionAssociable) wgPlayer, Flags.BUILD))) {
                                 event.setCancelled(true);
-                            } else {
+                            }
+                            // If can build allow interaction
+                            if (StateFlag.test(WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery().queryState(BukkitAdapter.adapt(currentLocation), (RegionAssociable) wgPlayer, Flags.BUILD))) {
                                 event.setCancelled(false);
                             }
+
                         }
                     }
                 }
